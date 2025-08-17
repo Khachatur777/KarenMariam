d2b6c92d1200f74b563a9a4bc8325b5b78f6652b
Khachatur Petrosyan <khachaturpetrosyan@conversebank.am>
2025-08-17 20:50:23 +0400

    test1

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 
-app.use(express.static('page'))
-app.use('/assets', express.static('assets'))
-
+// Настройка статических файлов
+app.use(express.static('page'));
+app.use('/assets', express.static('assets'));
+app.use('/page', express.static('page'));
 
 app.get('/join', (req, res) => {
     res.sendFile(__dirname + '/page/index.html');
@@ -15,9 +16,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
-
 app.use(cors());
-
 
 module.exports = app;
